82d661c7d4a47e892213e33cf71cf599df038f9d
aravula <aravula@IN-GPS107656>
2022-03-23 22:01:21 +0530

    Add project files.

---
+++ LoginForm/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace LoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Close = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.Username = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(188, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 22);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(167, 144);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(79, 35);
+            this.Close.TabIndex = 11;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(268, 144);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(75, 35);
+            this.Login.TabIndex = 10;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(287, 182);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(110, 16);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "ForgotPassWord";
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(51, 46);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(109, 20);
+            this.Username.TabIndex = 8;
+            this.Username.Text = "UserName :";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(51, 95);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(107, 20);
+            this.Password.TabIndex = 7;
+            this.Password.Text = "PassWord :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 232);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.Password);
+            this.Name = "Form1";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Label Password;
+    }
+}
+
---
+++ LoginForm/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (Form.ModifierKeys == Keys.None && keyData == Keys.Escape)
+            {
+                if
+                (MessageBox.Show("Quit the Application", "Exit Application Dialog", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Application.Exit();
+                    return true;
+                }
+            }
+            return base.ProcessDialogKey(keyData);
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+
+            if(CheckConnectivity())
+            {
+                Home home = new Home();
+                home.Show();
+                this.Hide();
+            }
+
+        }
+
+        private bool CheckConnectivity()
+        {
+                try
+                {
+                     using (OracleConnection con = new OracleConnection("Data Source=PT315PDB01_PIA;User Id=" + Username.Text + ";Password=" + Password.Text + ";"))
+                     {
+                           con.Open();
+                           return true;
+                     }
+                }
+                catch (OracleException ex)
+                {
+                    switch (ex.Number)
+                    {
+                        case 4060:
+                            MessageBox.Show("Invalid Database");                      
+                       break;
+                        case 18456:
+                        MessageBox.Show("Login Failed");
+                        break;
+                        default:
+                        MessageBox.Show("Unable to open Connection");
+                        break;
+                        //SELECT* FROM master.dbo.sysmessages
+
+
+                    }
+                      return false;
+                }
+            
+
+        }
+    }
+}
